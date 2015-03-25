af4f74eb43e326a808c1825418adc0da43c0c20f
Cam Jackson <camjackson89@gmail.com>
2015-03-25 16:46:07 +1100

    Got a window working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate glium;
+extern crate glutin;
+
 fn main() {
-    println!("Hello, world!");
+    use glium::DisplayBuild;
+
+    let display = glutin::WindowBuilder::new()
+        .with_dimensions(1024, 768)
+        .with_title(format!("Hello, world!"))
+        .build_glium()
+        .unwrap();
+
+    loop {
+        for event in display.poll_events() {
+            match event {
+                glutin::Event::Moved(x, y) => println!("Moved: ({}, {})", x, y),
+                glutin::Event::Closed => println!("Closed!"),
+                _ => ()
+            };
+            println!("Got an event");
+        }
+    }
 }
