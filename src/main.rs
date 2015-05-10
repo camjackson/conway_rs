e2c974bba0b836a7511c7a748f7cf52aa8f72b33
Cam Jackson <camjackson89@gmail.com>
2015-05-10 22:49:33 +1000

    Instancing yay

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 #[macro_use]
 extern crate glium;
 extern crate glutin;
-extern crate clock_ticks;
 
 use glium::Surface;
-
-use std::thread;
 
 mod shaders;
 mod shapes;
@@ -21,47 +18,29 @@ fn main() {
 		.build_glium()
 		.unwrap();
 
-	let (vertex_buffer, index_buffer) = shapes::square(&display);
+	let (vertices, instances, indices) = shapes::squares(&display);
 	let program = glium::Program::from_source(&display, &shaders::load("vertex"), &shaders::load("fragment"), None).unwrap();
 
-	let mut accumulator = 0;
-	let mut previous_clock = clock_ticks::precise_time_ns();
+	let uniforms = uniform! {
+		transform: [
+			[1.0 / width, 0.0         , 0.0, 0.0],
+			[0.0        , 1.0 / height, 0.0, 0.0],
+			[0.0        , 0.0         , 1.0, 0.0],
+			[0.0        , 0.0         , 0.0, 1.0f32]
+		]
+	};
 
 	loop {
-		let uniforms = uniform! {
-			transform: [
-				[1.0 / width, 0.0         , 0.0, 0.0],
-				[0.0        , 1.0 / height, 0.0, 0.0],
-				[0.0        , 0.0         , 1.0, 0.0],
-				[0.0        , 0.0         , 0.0, 1.0f32]
-			]
-		};
-
-		// drawing a frame
 		let mut frame = display.draw();
 		frame.clear_color(1.0, 1.0, 1.0, 1.0);
-		frame.draw(&vertex_buffer, &index_buffer, &program, &uniforms, &std::default::Default::default()).unwrap();
+		frame.draw((&vertices, instances.per_instance_if_supported().unwrap()), &indices, &program, &uniforms, &std::default::Default::default()).unwrap();
 		frame.finish();
 
-		// polling and handling the events received by the window
 		for event in display.poll_events() {
 			match event {
-					glutin::Event::Closed => return,
-					_ => ()
-				}
+				glutin::Event::Closed => return,
+				_ => ()
+			}
 		}
-
-		let now = clock_ticks::precise_time_ns();
-		accumulator += now - previous_clock;
-		previous_clock = now;
-
-		const FIXED_TIME_STAMP: u64 = 16666667;
-		while accumulator >= FIXED_TIME_STAMP {
-			accumulator -= FIXED_TIME_STAMP;
-
-			// if you have a game, update the state here
-		}
-
-		thread::sleep_ms(((FIXED_TIME_STAMP - accumulator) / 1000000) as u32);
 	}
 }
--- src/shapes.rs
+++ src/shapes.rs
@@ -2,33 +2,60 @@ extern crate glium;
 
 use glium::Display;
 use glium::vertex::VertexBufferAny;
+use glium::vertex::VertexBuffer;
 use glium::index::IndexBuffer;
 
+static SQUARE_SIZE: f32 = 20.0;
 
-pub fn square(display: &Display) -> (VertexBufferAny, IndexBuffer) {
-	(vb(display), ib(display))
+pub fn squares(display: &Display) -> (VertexBufferAny, VertexBuffer<Location>, IndexBuffer) {
+	(square(display), instances(display), ib(display))
 }
 
-#[derive(Copy, Clone)]
-struct Vertex {
-	position: [f32; 2],
-	color: [f32; 3],
-}
+fn square(display: &Display) -> VertexBufferAny {
+	#[derive(Copy, Clone)]
+	struct Vertex {
+		position: [f32; 2],
+		color: [f32; 3],
+	}
 
-implement_vertex!(Vertex, position, color);
+	implement_vertex!(Vertex, position, color);
 
-fn vb(display: &Display) -> VertexBufferAny {
-	let col = [0.1, 0.1, 0.1];
-	let size = 20.0;
+	let size = SQUARE_SIZE;
 
 	glium::VertexBuffer::new(display,
 		vec![
-			Vertex { position: [  0.0,  0.0], color: col },
-			Vertex { position: [  0.0, size], color: col },
-			Vertex { position: [ size, size], color: col },
-			Vertex { position: [ size,  0.0], color: col },
+			Vertex { position: [  0.0,  0.0], color: [1.0, 0.1, 0.1] },
+			Vertex { position: [  0.0, size], color: [0.1, 1.0, 0.1] },
+			Vertex { position: [ size, size], color: [0.1, 0.1, 1.0] },
+			Vertex { position: [ size,  0.0], color: [0.1, 1.0, 1.0] },
 		]
 	).into_vertex_buffer_any()
+}
+
+#[derive(Copy, Clone)]
+pub struct Location {
+	world_position: [f32; 2],
+}
+
+fn instances(display: &Display) -> VertexBuffer<Location> {
+	implement_vertex!(Location, world_position);
+
+	let grid_width = 102;
+	let grid_height = 76;
+
+	let mut data = Vec::new();
+	for x in (0i32 .. grid_width) {
+		for y in (0i32 .. grid_height) {
+			data.push(Location {
+				world_position: [
+					((x - grid_width / 2) as f32) * SQUARE_SIZE,
+					((y - grid_height / 2) as f32) * SQUARE_SIZE
+				]
+			});
+		}
+	}
+
+	glium::vertex::VertexBuffer::new(display, data)
 }
 
 fn ib(display: &Display) -> IndexBuffer {
