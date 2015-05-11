d2ee9018c98b494a5b095d077fa74cd9ca229116
Cam Jackson <camjackson89@gmail.com>
2015-05-11 18:29:38 +1000

    Replace tabs with spaces. The one true form of indentation.

--- src/main.rs
+++ src/main.rs
@@ -9,44 +9,44 @@ mod square;
 mod grid;
 
 fn main() {
-	use glium::DisplayBuild;
+    use glium::DisplayBuild;
 
-	let width = 1024.0;
-	let height = 768.0;
-	let display = glutin::WindowBuilder::new()
-		.with_dimensions(width as u32, height as u32)
-		.with_title(format!("Hello, world!"))
-		.build_glium()
-		.unwrap();
+    let width = 1024.0;
+    let height = 768.0;
+    let display = glutin::WindowBuilder::new()
+        .with_dimensions(width as u32, height as u32)
+        .with_title(format!("Hello, world!"))
+        .build_glium()
+        .unwrap();
 
-	let square_size = 32.0;
+    let square_size = 32.0;
 
-	let (vertices, indices) = square::geometry(&display, square_size);
-	let program = glium::Program::from_source(&display, &shaders::load("vertex"), &shaders::load("fragment"), None).unwrap();
+    let (vertices, indices) = square::geometry(&display, square_size);
+    let program = glium::Program::from_source(&display, &shaders::load("vertex"), &shaders::load("fragment"), None).unwrap();
 
-	let uniforms = uniform! {
-		transform: [
-			[1.0 / width, 0.0         , 0.0, 0.0],
-			[0.0        , 1.0 / height, 0.0, 0.0],
-			[0.0        , 0.0         , 1.0, 0.0],
-			[0.0        , 0.0         , 0.0, 1.0f32]
-		]
-	};
+    let uniforms = uniform! {
+        transform: [
+            [1.0 / width, 0.0         , 0.0, 0.0],
+            [0.0        , 1.0 / height, 0.0, 0.0],
+            [0.0        , 0.0         , 1.0, 0.0],
+            [0.0        , 0.0         , 0.0, 1.0f32]
+        ]
+    };
 
-	let grid = grid::new(64, 48, square_size);
-	let instances = square::instances(&display, grid);
+    let grid = grid::new(64, 48, square_size);
+    let instances = square::instances(&display, grid);
 
-	loop {
-		let mut frame = display.draw();
-		frame.clear_color(1.0, 1.0, 1.0, 1.0);
-		frame.draw((&vertices, instances.per_instance_if_supported().unwrap()), &indices, &program, &uniforms, &std::default::Default::default()).unwrap();
-		frame.finish();
+    loop {
+        let mut frame = display.draw();
+        frame.clear_color(1.0, 1.0, 1.0, 1.0);
+        frame.draw((&vertices, instances.per_instance_if_supported().unwrap()), &indices, &program, &uniforms, &std::default::Default::default()).unwrap();
+        frame.finish();
 
-		for event in display.poll_events() {
-			match event {
-				glutin::Event::Closed => return,
-				_ => ()
-			}
-		}
-	}
+        for event in display.poll_events() {
+            match event {
+                glutin::Event::Closed => return,
+                _ => ()
+            }
+        }
+    }
 }
--- src/shaders.rs
+++ src/shaders.rs
@@ -3,15 +3,15 @@ use std::fs::File;
 use std::io::prelude::*;
 
 pub fn load(name: &str) -> String {
-	let path = format!("shaders/{}.glsl", name);
-	let mut file = match File::open(&Path::new(&path)) {
-		Err(why) => panic!("Could not open shader '{}': {}", path, why),
-		Ok(file) => file
-	};
+    let path = format!("shaders/{}.glsl", name);
+    let mut file = match File::open(&Path::new(&path)) {
+        Err(why) => panic!("Could not open shader '{}': {}", path, why),
+        Ok(file) => file
+    };
 
-	let mut shader_text = String::new();
-	match file.read_to_string(&mut shader_text) {
-		Err(why) => panic!("Could not read vertex shader: {}", why),
-		Ok(_) => shader_text
-	}
+    let mut shader_text = String::new();
+    match file.read_to_string(&mut shader_text) {
+        Err(why) => panic!("Could not read vertex shader: {}", why),
+        Ok(_) => shader_text
+    }
 }
